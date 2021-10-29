56122b8f26be54e34730df627ffa2a97a88647bf
Rickard Ranniger <rickard.ranniger@pulsen.se>
2021-10-29 19:45:42 +0200

    first commit

---
+++ SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+
+namespace SortingAlgorithms
+{
+    public class BubbleSort
+    {
+        public int[] Sort(int[] numbers)
+        {
+            var length = numbers.Length-1;
+            for (var j = 0; j < length; j++)
+            for (var i = 0; i < length-j; i++)
+                if (numbers[i] > numbers[i+1])
+                    (numbers[i], numbers[i + 1]) = (numbers[i + 1], numbers[i]);
+
+            return numbers;
+        }
+    }
+}
---
+++ SortingAlgorithmsTest/SorterTest.cs
@@ -0,0 +1,18 @@
+using System;
+using SortingAlgorithms;
+using Xunit;
+
+namespace SortingAlgorithmsTest
+{
+    public class BubbleSortTest
+    {
+        [Fact]
+        public void TestCanBubbleSort()
+        {
+            var bubbleSort = new BubbleSort();
+            Assert.Equal(bubbleSort.Sort(
+                    new int[] {5, 4, 1, 2, 9, 7, 8, 6, 3}),
+                new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9});
+        }
+    }
+}
